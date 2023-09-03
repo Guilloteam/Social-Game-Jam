cca49fbfd3b7f318afac55490e3f2087904f499f
Clément <clement.guilloteam@gmail.com>
2023-09-03 12:35:59 +0200

    Update CV and configs

--- Assets/Scripts/UI/CvDisplay.cs
+++ Assets/Scripts/UI/CvDisplay.cs
@@ -8,6 +8,7 @@ public class CvDisplay : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI name;
     public TMPro.TextMeshProUGUI age;
+    public TMPro.TextMeshProUGUI french_level;
     public TMPro.TextMeshProUGUI diplome;
     public TMPro.TextMeshProUGUI experience;
     public TMPro.TextMeshProUGUI contact;
@@ -22,6 +23,8 @@ public class CvDisplay : MonoBehaviour
             name.text = cv_config.name;
         if(age != null)
             age.text = cv_config.age;
+        if(french_level != null)
+            french_level.text = cv_config.french_level;
         if(diplome != null)
             diplome.text = cv_config.diplome;
         if(experience != null)
